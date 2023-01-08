477ffde8ce92a2475bbd3e03bc7f6a21129a43ed
benyakir <benyakir.horowitz@gmail.com>
2023-01-08 01:35:29 -0700

    begin to configure users

--- src/store/user/store.ts
+++ src/store/user/store.ts
@@ -1,5 +1,15 @@
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
+import { User } from '@types'
+
 export const useUserStore = defineStore('config', () => {
-  return {}
+  const user = ref<null | User>(null)
+  function logout() {
+    user.value = null
+  }
+  function login(newUser: User) {
+    user.value = newUser
+  }
+  return { user, logout, login }
 })
